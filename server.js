59da3735fe5cf3dd23b67547361ae2d537aa4fb8
moitri-hazra <moitrihazra2000@gmail.com>
2023-12-14 18:51:46 +0530

    Added limit

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
 const routes = require('./routes/weatherRoutes');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const PORT = process.env.port || 8080;
 const WEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
 
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100 
+  });
+
+
 app.use(express.json());
 app.use(cors());
+app.use(apiLimiter);
 
 mongoose
 .connect(process.env.MONGODB_URL) 
